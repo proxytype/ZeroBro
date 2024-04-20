50c90fd73ae4f06aadeadcbbaa3cab2749493636
proxytype <29818286+proxytype@users.noreply.github.com>
2024-04-21 00:08:43 +0300

    Update lvgl_demo_ui.c

--- Sections/I2C- OLED/main/lvgl_demo_ui.c
+++ Sections/I2C- OLED/main/lvgl_demo_ui.c
@@ -14,11 +14,6 @@ void example_lvgl_demo_ui(lv_disp_t *disp)
     lv_label_set_long_mode(label, LV_LABEL_LONG_SCROLL_CIRCULAR); /* Circular scroll */
     lv_obj_set_style_text_font(label, &lv_font_montserrat_20, 0);
     lv_label_set_text(label, "        ZeroBRO        ");
-
-    int *size;
-    size = 28;
-    //lv_obj_set_style_text_color(label, lv_color_white(), LV_PART_MAIN);
-    //lv_obj_set_style_text_font(label, size, 0);
     lv_obj_set_width(label, 100);
 
     lv_obj_align(label, LV_ALIGN_CENTER, 0, 0);
